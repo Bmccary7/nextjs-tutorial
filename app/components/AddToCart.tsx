4b4c940b2168c87c4c85b6121f6c397c44522c88
Brandon McCary <mccarymid7@gmail.com>
2024-05-21 11:55:13 -0500

    added comment from video

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 const AddToCart = () => {
   return (
     <div>
+      {/* in-line styling using Tailwind */}
       <button className='btn btn-primary'onClick={() => console.log('Click')}>Add to cart</button>
     </div>
   )
